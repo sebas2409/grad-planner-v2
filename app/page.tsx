871a03d2656f97aea6688870bb5da995c4971ccd
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-01-05 19:39:30 -0700

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import {Fragment} from "react";
 import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
 
 export default function Home() {
     return (
         <Fragment>
-            <h1>Home Page</h1>
-            <Link href='/myPlans'>My Plans</Link>
-            <Link href='/planDetails'>Plan Details</Link>
+            <div className="min-h-screen w-full bg-gray-50 font-sans text-slate-800 flex flex-col">
+                <main className="flex-1 w-full">
+                    <div className="mx-auto max-w-6xl px-4 py-8 space-y-6">
+                        <div className="flex gap-2 ">
+                            <Link href='/myPlans' className={buttonVariants({variant: 'default'})}>My Plans</Link>
+                            <Link href='/planDetails' className={buttonVariants({variant: 'default'})}>Plan
+                                Details</Link>
+                        </div>
+                    </div>
+
+                </main>
+
+            </div>
+
         </Fragment>
     );
 }
